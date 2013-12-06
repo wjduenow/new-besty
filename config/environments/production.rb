3ac91230dcba7285092407ce0b986a36314838c5
Wes Duenow <wjduenow@raybeam.com>
2013-12-06 13:23:06 -0800

    secondt take on mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ NewBesty::Application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    :address => "email-smtp.us-east-1.amazonaws.comv",
+    :address => "email-smtp.us-east-1.amazonaws.com",
     :port => 465,
     :domain => "friend-os.com",
     :authentication => :login,
