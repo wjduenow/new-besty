92ac6a60bad3be5bab442a21eafa1ae02d812233
Wes Duenow <wjduenow@raybeam.com>
2013-12-08 19:42:53 -0800

    Work on Profile 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,8 @@ class UsersController < ApplicationController
 
   def show
 
-    description = <<EOF I recently relocated to San Francisco from Ireland.  Although I am a family man, I really miss my mates back home.  I have tried to find new friends through work, but I really want to find a friendship that doesn't have anything to do with my 9-5.
-
-
-I am eaasy going and a great listener, at least my mother always tells me that is the case.  If you would rather listen than talk I can do that too.  I have a wonderful Irish accent and it makes even my boring stories sound interesting.  Not that I have any boring stories .. ha ha ha!  
-
-  Just give me a chance I am sure I could be a great friend.
-
-  I am so glad to have found this fantastic website and relieved to know there are people like me out there.
-
-  <strong>We are NOT alone</strong>!
-
-  Anyhoo ... give me a shout if your interested in meeting up.
-EOF
+    str_desc = "I recently relocated to San Francisco from Ireland.  Although I am a family man, I really miss my mates back home.  I have tried to find new friends through work, but I really want to find a friendship that doesn't have anything to do with my 9-5.<br><br>I am eaasy going and a great listener, at least my mother always tells me that is the case.<br><br> If you would rather listen than talk I can do that too.  I have a wonderful Irish accent and it makes even my boring stories sound interesting.  Not that I have any boring stories .. ha ha ha! <br><br>Just give me a chance I am sure I could be a great friend.<br><br>I am so glad to have found this fantastic website and relieved to know there are people like me out there.<br><br><strong>We are NOT alone</strong>!<br><br>Anyhoo ... give me a shout if your interested in meeting up."
+    
   	@user = User.new(:fname => "Robert", 
                      :lname => "Gibbs", 
                      :nname => "Gibby",
@@ -45,12 +34,9 @@ EOF
                      :loneliness_score => "75%",
                      :phone_days_a_week => "5-7",
                      :friend_days_a_weeek => "1-2",
-
-
-                      :reason_since => "I moved here from Ireland",
-                      :description => description,
-                      :looking_for => "Companionship",
-
+                     :reason_since => "I moved here from Ireland",
+                     :description => str_desc,
+                     :looking_for => "Companionship",
                      :profile_image => "/img/gibbs.png")
 
 
